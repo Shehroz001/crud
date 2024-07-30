a64c6112dc47a44aa72a4c7cee9dc8fe9f02e6ca
Shehroz001 <shehrozt455@gmail.com>
2024-07-31 02:04:35 +0500

    update frontend

---
+++ app/editTopic/[id]/page.jsx
@@ -0,0 +1,14 @@
+import EditTopicForm from '@/components/EditTopicForm'
+import React from 'react'
+
+const EditTopic = () => {
+  return (
+    <form className="flex flex-col gap-3"action="">
+      <input className="border border-slate-500 px-8 py-2"type="text" placeholder="Topic Title" />
+      <input className="border border-slate-500 px-8 py-2"type="text" placeholder="Topic Description" />
+      <button className="bg-green-600 font-bold text-white px-6 py-3 w-fit ">Update Topic</button>
+    </form>
+  )
+}
+
+export default EditTopic
---
+++ components/EditTopicForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditTopicForm = () => {
+  return (
+    <div>EditTopicForm</div>
+  )
+}
+
+export default EditTopicForm
