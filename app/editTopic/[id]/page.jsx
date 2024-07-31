ed5d66eb06871d1a908281a4a5b0778f84ce0605
Shehroz001 <shehrozt455@gmail.com>
2024-07-31 17:57:33 +0500

    Backend to frontend

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,14 +1,26 @@
 import EditTopicForm from '@/components/EditTopicForm'
-import React from 'react'
 
-const EditTopic = () => {
-  return (
-    <form className="flex flex-col gap-3"action="">
-      <input className="border border-slate-500 px-8 py-2"type="text" placeholder="Topic Title" />
-      <input className="border border-slate-500 px-8 py-2"type="text" placeholder="Topic Description" />
-      <button className="bg-green-600 font-bold text-white px-6 py-3 w-fit ">Update Topic</button>
-    </form>
-  )
+const getTopicById = async ( id ) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/topics/${ id }`,{cache: 'no-store',});
+
+    if(!res.ok){
+      throw new Error("Failed to fetch topic");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const EditTopic = async ({ params }) => {
+  const { id } = params;
+  
+  const { topic } = await getTopicById(id);
+  const{ title,description } = topic;
+  
+  console.log("id:", id);
+  return <EditTopicForm id={id} title={title} description={description}/>;
 }
 
 export default EditTopic
